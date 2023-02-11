afbcb171f7964f9ae8a7055358b70a3f6df9926e
Kent Zysk <kent@kentzysk.com>
2023-02-11 00:59:58 -0600

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Initialize Variables
+        double itemCost = 99;
+        double shippingCost = 0;
+        double totalCost = 0;
+        double totalShipped = 0;
+            //If the item costs less than 100, add shipping cost.
+            if (itemCost < 100) {
+                shippingCost = itemCost * .2;
+                totalCost = itemCost + shippingCost;
+            } else {
+                totalCost = itemCost;
+            }
+        System.out.println("Your shipping cost is $" + shippingCost + " and your total cost is $" + totalCost + ".");
+
     }
 }
